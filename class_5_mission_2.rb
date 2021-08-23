e557059c0708b5e190b61e15ccf810bfa891967b
RaquelLima7 <raquellimadev@gmail.com>
2021-08-23 17:54:30 -0300

    class 5

---
+++ class_5_mission_2.rb
@@ -0,0 +1,21 @@
+# Missão 2
+
+# Siga a documentação da gem cpf_cnpj para criar um programa que recebe como entrada um número de cpf 
+# e em um método verifique se este número é válido.
+
+require "cpf_cnpj"
+
+def check_cpf(cpf_number)
+  if CPF.valid?(cpf_number)
+    return "O cpf informado é valido."
+  else
+    return "O cpf informado é invalido."
+  end
+end
+
+print 'Digite seu CPF: '
+cpf_number = gets.chomp.to_i
+
+result = check_cpf(cpf_number)
+
+puts result
